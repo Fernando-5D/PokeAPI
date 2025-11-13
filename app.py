98b4e4dbe61b306f4a2ac66da56c15fa22df11ba
Fernando-5D <23308060610105@cetis61.edu.mx>
2025-11-12 20:22:04 -0700

    commit

--- app.py
+++ app.py
@@ -2,6 +2,30 @@ import requests
 from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
+app.config["SECRET_KEY"] = "pokeapi"
+badges = {
+    "normal": "",
+    "fighting": "",
+    "flying": "",
+    "poison": "",
+    "ground": "",
+    "rock": "",
+    "bug": "",
+    "ghost": "",
+    "steel": "",
+    "fire": "",
+    "water": "",
+    "grass": "",
+    "electric": "",
+    "psychic": "",
+    "ice": "",
+    "dragon": "",
+    "dark": "",
+    "fairy": "",
+    "stellar": "",
+    "unknown": "",
+}
+
 @app.route("/")
 def form():
     return render_template("form.html")
@@ -21,15 +45,16 @@ def buscarPoke():
                         "spriteShiny": resp["sprites"]["front_shiny"],
                         "id": resp["id"],
                         "name": resp["name"],
-                        "types": resp["types"]
+                        "types": resp["types"],
+                        "stats": resp["stats"]
                     }
                     
-                    return render_template("pokemon.html", pokemon = pokemon)
+                    return render_template("pokemon.html", pokemon = pokemon, badges = badges)
                 else:
-                    flash(f"No se encontro el Pokemon: {nombrePok}")
+                    flash(f"No se encontro el Pokemon: {str(nombrePok).title()}")
                     return render_template("form.html")
             except requests.exceptions.RequestException as err:
-                flash(f"Ocurrio un error al buscar al Pokemon: {nombrePok}")
+                flash(f"Ocurrio un error al buscar al Pokemon: {str(nombrePok).title()}")
                 return render_template("form.html")
         else:
             return redirect(url_for("form"))
